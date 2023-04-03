42e9a6a10b5a53c3a9b57b80ebaf566f86dac512
Pallavi Tilloo <72088607+pallavitilloo@users.noreply.github.com>
2023-04-03 15:13:51 -0400

    Update main.cpp
    Adding code to get coordinates from user

--- src/main.cpp
+++ src/main.cpp
@@ -52,9 +52,14 @@ int main(int argc, const char **argv)
             osm_data = std::move(*data);
     }
     
-    // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
-    // user input for these values using std::cin. Pass the user input to the
-    // RoutePlanner object below in place of 10, 10, 90, 90.
+    // Get user input and pass the values to the RoutePlanner object 
+    float start_x, start_y, end_x, end_y;
+  	      
+    std::cout << "Enter the starting coordinates [0, 100]: ";
+    std::cin >> start_x >> start_y;
+
+    std::cout <<"\nEnter the ending coordinates [0, 100]: ";
+    std::cin >> end_x >> end_y;
 
     // Build Model.
     RouteModel model{osm_data};
